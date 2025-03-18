49f2f49007a9ce61c7faf528310dde7caaff6f24
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2025-03-18 08:20:14 +0700

    Godsave Push

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const {createMenuProduct, getMenuProducts, getSingleMenuProduct, deleteMenuProduct, updateMenuProduct} = require('../controller/productController')
-const {menuStorage} = require('../configuration/productConfiguration')
+const { menuStorage } = require('../configuration/productConfiguration')
 
 router.post('/createMenuProduct', menuStorage, createMenuProduct)
 router.get('/getMenuProducts', getMenuProducts)
