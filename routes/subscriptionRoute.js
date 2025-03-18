8bea17cd64b80a42d576c42b0c0bd325a32e0ba6
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2025-03-18 08:35:12 +0700

    Test pushing

--- routes/subscriptionRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {subscribe} = require('../controller/orderController')
-
-router.post('/subscribe', subscribe)
-
-module.exports = router
